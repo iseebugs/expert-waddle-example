a419ba55507e0fc41e350ecd33561fe60626adea
Sergeeva Anastasiia <a.serg@topface.com>
2019-08-17 23:09:15 +0300

    Initial Commit

---
+++ NewsReaderTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsReaderTestApp
+//
+//  Created by Anastasija Sergeeva on 17/08/2019.
+//  Copyright © 2019 Anastasija Sergeeva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
